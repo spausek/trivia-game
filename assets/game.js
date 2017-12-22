fafd234789396031b9f084b444289defad20acf0
spausek <31174081+spausek@users.noreply.github.com>
2017-12-21 22:05:07 -0800

    Completed
    Added styling  to divs and a background. Added a finished button incase player finishes before the timer expires.

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 
-	var timerInSeconds = 10;
+	var timerInSeconds = 60;
 	var correctAnswers = 0;
 	var wrongAnswers = 0;
 	var unanswered = 0;
+	var gameStarted = false;
 
 	
 
 	$('#questions-display').hide();
+	$('#end-game-display').hide();
 
 	
 
@@ -23,11 +25,15 @@ $(document).ready(function() {
 	});
 
 	function timer (){
+		gameStarted = true;
+		if (gameStarted === true) {
 		timerInSeconds --;
 		$('#timer-display').text(timerInSeconds + ' ' + 'Seconds');
+	}
 		if (timerInSeconds === -1) {
-			//display end div
+		
 			endGame();
+		
 
 		}
 
@@ -41,17 +47,36 @@ $(document).ready(function() {
 
 	}
 
-	function endGame() {
+	$('.finished').on('click', function() {
 
-		//show end div
+		endGame();
+
+	});
+
+	function endGame() {
+		
+		gamestarted = false;
 		var question1 = $('input:radio[name="question1"]:checked').val();
+		var question2 = $('input:radio[name="question2"]:checked').val();
+		var question3 = $('input:radio[name="question3"]:checked').val();
+		var question4 = $('input:radio[name="question4"]:checked').val();
+		var question5 = $('input:radio[name="question5"]:checked').val();
+		var question6 = $('input:radio[name="question6"]:checked').val();
+		var question7 = $('input:radio[name="question7"]:checked').val();
+		var question8 = $('input:radio[name="question8"]:checked').val();
+		var question9 = $('input:radio[name="question9"]:checked').val();
+		var question10 = $('input:radio[name="question10"]:checked').val();
+
+		$('#end-game-display').show();
+		$('#questions-display').hide();
+		
 
 		if (question1 === 'Romulus and Remus') {
-			correcAnswers ++;
+			correctAnswers ++;
 		}
 
 		else if (question1 === undefined){
-			wrongAnswers ++;
+			unanswered ++;
 		}
 		
 
@@ -59,7 +84,138 @@ $(document).ready(function() {
 			wrongAnswers ++;
 
 		}
-		console.log(wrongAnswers);
+		
+
+		if (question2 === 'Lucius Tarquinius Superbus') {
+			correctAnswers ++;
+		}
+
+		else if (question2 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question3 === 'Carthage') {
+			correctAnswers ++;
+		}
+
+		else if (question3 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question4 === 'Zama') {
+			correctAnswers ++;
+		}
+
+		else if (question4 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question5 === 'Dictator for Life') {
+			correctAnswers ++;
+		}
+
+		else if (question5 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question6 === 'Pompeii') {
+			correctAnswers ++;
+		}
+
+		else if (question6 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question7 === 'The Rubicon') {
+			correctAnswers ++;
+		}
+
+		else if (question7 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question8 === 'Augustus Caesar') {
+			correctAnswers ++;
+		}
+
+		else if (question8 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question9 === 'Claudius') {
+			correctAnswers ++;
+		}
+
+		else if (question9 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		if (question10 === 'Byzantium') {
+			correctAnswers ++;
+		}
+
+		else if (question10 === undefined){
+			unanswered ++;
+		}
+		
+
+		else {
+			wrongAnswers ++;
+
+		}
+
+		$('#right-answers').html(correctAnswers);
+		$('#wrong-answers').html(wrongAnswers);
+		$('#unanswered-questions').html(unanswered);
+
 
 
 
